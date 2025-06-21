92b8c199370f351a768fb0caa81c9a9a06b2b31e
ithinkiborkedit <miles.warburton@gmail.com>
2025-06-21 15:12:11 +0200

    SDFDSF

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -308,10 +308,7 @@ func (l *Lexer) scanToken() {
 	case '"', '\'':
 		l.string(ch)
 	case '\n':
-		if len(l.tokens) == 0 || l.tokens[len(l.tokens)-1].Type != token.TokenNewLine {
-			l.addToken(token.TokenNewLine)
-		}
-		// l.addToken(token.TokenNewLine)
+		l.addToken(token.TokenNewLine)
 		l.line++
 		l.column = 0
 	case ' ', '\r', '\t':
