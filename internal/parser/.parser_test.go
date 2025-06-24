23871c7f4facf40dd68ef9059b1a3a016cbfe155
ithinkiborkedit <miles.warburton@gmail.com>
2025-06-24 22:29:00 +0200

    Update .parser_test.go

--- internal/parser/.parser_test.go
+++ internal/parser/.parser_test.go
@@ -4,41 +4,67 @@ import (
 	"testing"
 
 	"github.com/ithinkiborkedit/niftelv2.git/internal/lexer"
+	"github.com/ithinkiborkedit/niftelv2.git/internal/niftokens"
 	"github.com/ithinkiborkedit/niftelv2.git/internal/parser"
+	"github.com/ithinkiborkedit/niftelv2.git/internal/nifast"
 )
 
-func TestParser_Basic(t *testing.T) {
-	source := `	
-	func add(a: int, b: int) -> int {
-	   return a + b
+func lexAll(source string) []niftokens.Token {
+	lex := lexer.New(source)
+	var tokens []niftokens.Token
+	for {
+		tok, err := lex.NextToken()
+		if err != nil {
+			panic(err)
+		}
+		if tok.Type == niftokens.TokenEOF {
+			tokens = append(tokens, tok)
+			break
+		}
+		tokens = append(tokens, tok)
 	}
-	var result: int = add(5,3)
+	return tokens
+}
 
-	if result > 5 {
-	  print("success")
-
-	} else {
-	  print("fail") 
-	}
-	// single line comment
-	/* block
-	   comment */
+func TestParser_Parse(t *testing.T) {
+	source := `
+		var x: int = 42
+		var y: int = x + 1
+		print(x)
 	`
-
-	l := lexer.New(source)
-	tokens := l.ScanTokens()
-
-	for i, tok := range tokens {
-		t.Logf("%30d: %s\t%q", i, tok.Type, tok.Lexeme)
-	}
+	tokens := lexAll(source)
 
 	p := parser.New(tokens)
-
-	program, err := p.Parse()
+	stmts, err := p.Parse()
 	if err != nil {
-		t.Fatalf("Parser error: %v", err)
+		t.Fatalf("Parse failed: %v", err)
 	}
 
-	t.Logf("AST %+v", program)
+	if len(stmts) != 3 {
+		t.Fatalf("Expected 3 statements, got %d", len(stmts))
+	}
 
+	// Check first statement: var x: int = 42
+	var1, ok := stmts[0].(*nifast.VarStmt)
+	if !ok {
+		t.Fatalf("First stmt is not VarStmt, got %T", stmts[0])
+	}
+	if var1.Name.Lexeme != "x" {
+		t.Errorf("Expected var name 'x', got %q", var1.Name.Lexeme)
+	}
+
+	// Check second statement: var y: int = x + 1
+	var2, ok := stmts[1].(*nifast.VarStmt)
+	if !ok {
+		t.Fatalf("Second stmt is not VarStmt, got %T", stmts[1])
+	}
+	if var2.Name.Lexeme != "y" {
+		t.Errorf("Expected var name 'y', got %q", var2.Name.Lexeme)
+	}
+
+	// Check third statement: print(x)
+	printStmt, ok := stmts[2].(*nifast.PrintStmt)
+	if !ok {
+		t.Fatalf("Third stmt is not PrintStmt, got %T", stmts[2])
+	}
 }
