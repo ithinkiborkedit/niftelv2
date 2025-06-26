598f7d0453e8891ceb72fc975710f12836fbd0b0
ithinkiborkedit <miles.warburton@gmail.com>
2025-06-26 16:00:39 +0200

    sdfd

--- internal/environment/environment.go
+++ internal/environment/environment.go
@@ -9,12 +9,14 @@ import (
 type Environment struct {
 	variables map[string]value.Value
 	enclosing *Environment
+	types     map[string]*value.StructType
 }
 
 func NewEnvironment(enclosing *Environment) *Environment {
 	return &Environment{
 		variables: make(map[string]value.Value),
 		enclosing: enclosing,
+		types:     make(map[string]*value.StructType),
 	}
 }
 
@@ -22,6 +24,8 @@ func (e *Environment) Define(name string, val value.Value) {
 	fmt.Printf("ENV DEFINING '%s' as type %v", name, val.Type)
 	e.variables[name] = val
 }
+
+func (e *Environment) Register(name string, typ *value.Value) {}
 
 func (e *Environment) Assign(name string, val value.Value) error {
 
