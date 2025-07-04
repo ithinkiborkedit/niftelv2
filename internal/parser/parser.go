0ebff81414dc3d8e004f657aba7bd13a9713d816
ithinkiborkedit <miles.warburton@gmail.com>
2025-07-04 13:56:57 +0200

    Sdsdf

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -728,13 +728,7 @@ func (p *Parser) primaryExpr() (ast.Expr, error) {
 			}
 			return p.structLiteralExpr(typeExpr)
 		}
-		// typeExpr, err := p.parseTypeExprFromToken(p.prev)
-		// if err != nil {
-		// 	return nil, err
-		// }
-		// if p.check(token.TokenLBrace) {
-		// 	return p.structLiteralExpr(typeExpr)
-		// }
+
 		return &ast.VariableExpr{Name: p.prev}, nil
 	}
 	ok, err = p.match(token.TokenIdentifier)
@@ -754,13 +748,7 @@ func (p *Parser) primaryExpr() (ast.Expr, error) {
 		}
 		return &ast.VariableExpr{Name: typeExpr.Name}, nil
 	}
-	// ok, err = p.match(token.TokenIdentifier)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// if ok {
-	// 	return &ast.VariableExpr{Name: p.prev}, nil
-	// }
+
 	ok, err = p.match(token.TokenLParen)
 	if err != nil {
 		return nil, err
